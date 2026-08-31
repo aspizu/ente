09c4319c5272f6b86de26be0b24acd26040e1307
Priyanshu Dangare <priyanshu@ente.com>
2026-08-31 20:23:27 +0530

    [mobile][photos] add placeholder UI for memory lane strip card

--- mobile/apps/photos/lib/ui/home/memories/memory_lane_card.dart
+++ mobile/apps/photos/lib/ui/home/memories/memory_lane_card.dart
@@ -1,9 +1,11 @@
+import "dart:convert";
 import "dart:typed_data";
 
 import "package:flutter/material.dart";
 import "package:photos/models/memory_lane/memory_lane_models.dart";
+import "package:photos/ui/home/memories/memory_card_constants.dart";
 
-class MemoryLaneCardWidget extends StatefulWidget {
+class MemoryLaneCardWidget extends StatelessWidget {
   final MemoryLanePersonTimeline timeline;
   final Uint8List oldestFace;
   final Uint8List newestFace;
@@ -16,24 +18,82 @@ class MemoryLaneCardWidget extends StatefulWidget {
   });
 
   @override
-  State<MemoryLaneCardWidget> createState() => _MemoryLaneCardWidgetState();
+  Widget build(BuildContext context) {
+    final timelineDump = const JsonEncoder.withIndent(
+      "  ",
+    ).convert(timeline.toJson());
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kMemoryCardStripGap / 2),
+      child: Container(
+        width: 320,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(kMemoryCardBorderRadius),
+        ),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 96,
+              child: Row(
+                children: [
+                  Expanded(child: _FaceDebugView("oldest", oldestFace)),
+                  Expanded(child: _FaceDebugView("newest", newestFace)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(6),
+                child: Text(
+                  timelineDump,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: "monospace",
+                    fontSize: 8,
+                    height: 1.1,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _MemoryLaneCardWidgetState extends State<MemoryLaneCardWidget> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
+class _FaceDebugView extends StatelessWidget {
+  final String label;
+  final Uint8List bytes;
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
+  const _FaceDebugView(this.label, this.bytes);
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox.shrink();
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Image(
+          image: MemoryImage(bytes),
+          fit: BoxFit.cover,
+          gaplessPlayback: true,
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: ColoredBox(
+            color: Colors.black54,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 2),
+              child: Text(
+                label,
+                style: const TextStyle(color: Colors.white, fontSize: 10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
